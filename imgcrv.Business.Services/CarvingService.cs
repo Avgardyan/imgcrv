471b8be7bcf70b4472922e99818cb1a3eb65f80b
Avgardyan <augustasadom@gmail.com>
2014-04-01 07:28:32 +0300

    Changed services to be more servicey, change ImageEntity class.

--- imgcrv.Business.Services/CarvingService.cs
+++ imgcrv.Business.Services/CarvingService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ImageMagick;
+using imgcrv.Data.DataEntities.Dto;
 
 namespace imgcrv.Business.Services
 {
@@ -13,9 +14,16 @@ namespace imgcrv.Business.Services
         private MagickImage image;
 
         
+        double PixelToPercent(int before, int after)
+        {
+            return (double)after / before;
+        }
 
+        ImageEntity Carve(ImageEntity image, int height, int width)
+        {
 
-        
+            return image;
+        }
         
     }
 }
--- imgcrv.Data.DataEntities/Dto/ImageEntity.cs
+++ imgcrv.Data.DataEntities/Dto/ImageEntity.cs
@@ -3,26 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using ImageMagick;
 
 namespace imgcrv.Data.DataEntities.Dto
 {
-    class ImageEntity
+    public class ImageEntity
     {
-        private MagickImage img;
-        int height;
-        int width;
+        int height { get; private set; }
+        int width { get; private set; }
+        private MagickImage image;
+        private string imageUrl;
 
-        public ImageEntity(MagickImage image)
+        public ImageEntity(string imageUrl)
         {
-            img = image;
-            height = img.Height;
-            width = img.Width;
+            this.image = new MagickImage(imageUrl);
+            SetSize();
+        }
+
+        public ImageEntity(MagickImage image, string imageUrl)
+        {
+            this.image = image;
+            SetSize();
+        }
+
+        private void SetSize()
+        {
+            //exception
+            this.height = image.Height;
+            this.width = image.Width;            
         }
 
         MagickImage GetMagickImage()
         {
-            return img;
+            if (image != null)
+                return image;
+            return null;
+            //Exception here?
+        }
+
+        void SetMagickImage(MagickImage image)
+        {
+            this.image = image;
+        }
+
+        void SetImageUrl(string imageUrl)
+        {
+            this.imageUrl = imageUrl;
+        }
+
+        void ReadImageFromUrl()
+        {
+            this.image = new MagickImage(imageUrl);
+        }
+
+        int GetHeight()
+        {
+            return height;
+        }
+
+        int GetWidth()
+        {
+            return width;
         }
     }
 }
