aa46cdbe1aec1a0d3f34b039dabf1ddb06b0fd2b
Andrius <a.vasiukevicius@gmail.com>
2014-04-21 17:19:36 +0300

    Added file uploading.

--- imgcrv.Presentation.Web/Controllers/HomeController.cs
+++ imgcrv.Presentation.Web/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using imgcrv.Business.Services;
+using System.IO;
 
 namespace imgcrv.Presentation.Web.Controllers
 {
@@ -11,6 +13,21 @@ namespace imgcrv.Presentation.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            FileHandlerService Fhandler = new FileHandlerService();
+            if (file.ContentLength > 0)
+            {
+
+                string fileName = Fhandler.GenerateRandomName() + Path.GetExtension(file.FileName);
+                string path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
+
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
